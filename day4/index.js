d03b106ec7266018dd21d58b76c56d0bfa2ec394
Tyler Harnach <harnatc@gmail.com>
2022-12-22 09:45:58 -0600

    day 4 part 1

---
+++ day4/index.js
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import chalk from 'chalk';
+
+const stringArray = fs.readFileSync('./input.txt')
+// const stringArray = fs.readFileSync('./sample.txt')
+    .toString()
+    .split("\n");
+
+
+const fullyCovered = stringArray.reduce((acc, curr) => {
+    const [s1, s2] = curr.split(',')
+    const [s1s, s1e] = s1.split('-')
+    const [s2s, s2e] = s2.split('-')
+    const s1Start = Number(s1s)
+    const s1End = Number(s1e)
+    const s2Start = Number(s2s)
+    const s2End = Number(s2e)
+    if ((s1Start <= s2Start && s1End >= s2End) || (s2Start <= s1Start && s2End >= s1End)) {
+        return acc += 1
+    }
+    return acc
+}, 0)
+
+console.log(chalk.redBright('fullyCovered: ', fullyCovered))
